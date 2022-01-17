f78a782c20c91e2a6edcba942cc108df02389e4e
claudio.coimbra <claudiojccoimbra@gmail.com>
2022-01-17 09:45:35 -0300

    referencia do contexto na camada webapi

---
+++ IdentidadeDigital.WebApi/OracleDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdentidadeDigital.WebApi
+{
+    public class OracleDbContext : DbContext
+    {
+        public OracleDbContext(DbContextOptions options) : base(options) { }
+    }
+}
